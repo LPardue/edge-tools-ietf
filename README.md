44d6d400a51fc786bdf9e7744786253b2b100cc3
Lucas Pardue <lucaspardue.24.7@gmail.com>
2020-09-16 14:34:50 +0100

    Add support for www.rfc-editor.org

--- README.md
+++ README.md
@@ -6,7 +6,19 @@ I pity the tools!
 
 # Description
 
-A pretty simple Cloudflare Workers script to speed up access to resources on tools.ietf.org. For example, try visiting https://tools-ietf-org.lucaspardue.com/html/rfc7540 a couple of times with local caching disabled and see how much faster life can be when modern edge capabilities are harnessed.
+A pretty simple Cloudflare Workers script to speed up access to resources on tools.ietf.org and www.rfc-editor.org. For example, try visiting https://tools-ietf-org.lucaspardue.com/html/rfc7540 a couple of times with local caching disabled and see how much faster life can be when modern edge capabilities are harnessed.
+
+# Converting hosts
+
+All you need to do is take a hostname, convert the dots to hyphens and append lucaspardue.com. Any path after that should work. The follow table lists supported substitute hosts
+
+| Original  | Substitute  |
+|---|---|---|---|---|
+| tools.ietf.org | tools-ietf-org.lucaspardue.com |
+| xml2rfc.tools.ietf.org | xml2rfc-tools-ietf-org.lucaspardue.com |
+| www.rfc-editor.org  | www-rfc-editor-org.lucaspardue.com  |
+
+
 
 Most paths on tools are probably supported but I've only tested viewing I-Ds and RFCs. Chances are other stuff could act funky, if so ping me and just fallback if needs be.
 
@@ -14,8 +26,7 @@ Most paths on tools are probably supported but I've only tested viewing I-Ds and
 
 This is early days. So far the worker is hardcoded to:
 
-1) Substitute for tools.ietf.org (tools-ietf-org.lucaspardue.com) or xml2rfc.tools.ietf.org (xml2rfc-tools-ietf-org.lucaspardue.com), and
+1) Do reverse substitution using the details above.
 2) Check the local edge cache for a copy and return it, or
-3) Fetch the canonical copy from tools.ietf.org, rewrite some links to RFC editor documents, cache that version in the local edge cache.
+3) Fetch the canonical copy from origin, rewrite some things (like links to RFC editor documents_, cache that version in the local edge cache.
 4) Return the resource.
-
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function handleRequest(event) {
   let url = new URL(event.request.url);
   
-  let pattern = /(tools-ietf-org|xml2rfc-tools-ietf-org|tools-ietf-production).(lucaspardue.com|lucas.worker.dev)/;
+  let pattern = /(tools-ietf-org|xml2rfc-tools-ietf-org|www-rfc-editor-org|tools-ietf-production).(lucaspardue.com|lucas.worker.dev)/;
 
   const found = url.hostname.match(pattern);
 
@@ -10,24 +10,51 @@ async function handleRequest(event) {
     return new Response(init);
   }
 
-  console.log(found);
-  url.hostname = found[1].replaceAll('-','.');
-  console.log(url.hostname);
-  console.log(url);
+  let rfcEditor = false;
+
+  // RFC Editor url includes a freakin' hyphen
+  if (found[1] == "www-rfc-editor-org") {
+    url.hostname = "www.rfc-editor.org";
+    rfcEditor = true;
+  } else {
+    url.hostname = found[1].replaceAll('-','.');
+    transform = true;
+  }
 
   let response
 
-  let cache = caches.default
-  // try to find the cache key in the cache
+  // Try to find the cache key in the cache.
+  // If it's not there, then we'll fetch it
+  // and optionally transform.
+  let cache = caches.default  
   response = await cache.match(event.request)
 
   if (!response) {
     response = await fetch(url,event.request);
 
-    let newResponse = rewriter.transform(response);
+    if (rfcEditor == true) {
+      if (url.pathname.endsWith(".json")) {
+        event.waitUntil(cache.put(event.request, response.clone()));        
+        return response;
+      }
 
-    event.waitUntil(cache.put(event.request, newResponse.clone()));
-    return newResponse
+      if (url.pathname == "/js/metadata.min.js") {
+        let text = await response.text();
+        let newResponse = new Response(text.replaceAll("https://www.rfc-editor.org", "https://www-rfc-editor-org.lucaspardue.com"), response);
+        event.waitUntil(cache.put(event.request, newResponse.clone()));
+
+        return newResponse;
+      }
+
+      let newResponse = rfcEfdRewriter.transform(response);
+
+      event.waitUntil(cache.put(event.request, newResponse.clone()));
+      return newResponse
+    } else {
+      let newResponse = rewriter.transform(response);
+      event.waitUntil(cache.put(event.request, newResponse.clone()));
+      return newResponse
+    }    
   }
 
   return response
@@ -53,6 +80,27 @@ const rewriter = new HTMLRewriter()
   .on('a', new AttributeRewriter('href'))
   .on('img', new AttributeRewriter('src'))
   //.on('pre', new ElementHandler())
+
+  class RfcEdAttributeRewriter {
+    constructor(attributeName) {
+        this.attributeName = attributeName
+    }
+  
+    element(element) {
+      const attribute = element.getAttribute(this.attributeName)
+      if (attribute) {
+        element.setAttribute(
+          this.attributeName,
+          attribute.replace('www.rfc-editor.org', 'www-rfc-editor-org.lucaspardue.com')
+        )
+      }
+    }
+  }
+  
+  const rfcEfdRewriter = new HTMLRewriter()
+    .on('script', new RfcEdAttributeRewriter('src'))
+    //.on('img', new AttributeRewriter('src'))
+    //.on('pre', new ElementHandler())
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
